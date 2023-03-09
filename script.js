7590af44be3d8d79dde30fd2b41c1cc255669f58
Chai Keshab <keshab3886@gmail.com>
2023-03-09 18:40:28 +0545

    weather api

---
+++ script.js
@@ -0,0 +1,26 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a4a659351bmshac2ff0d49921ddap159a72jsnd90fc21b1bd9',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=tokyo', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+
+        temp.innerHTML = response.temp
+        cloud_pct.innerHTML = response.cloud_pct
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+
+    })
+    .catch(err => console.error(err));
